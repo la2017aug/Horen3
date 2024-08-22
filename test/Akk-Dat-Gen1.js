e152ee3ced224695abcd85422da0c140b1fca378
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2024-08-22 12:39:24 +0200

    load blok test

---
+++ test/Akk-Dat-Gen1.js
@@ -0,0 +1,54 @@
+const taskDescription = `
+Ответьте на вопросы, употребляя в ответах данные в скобках существительные в нужном падеже.
+Beantworten Sie die Fragen, verwenden Sie in Ihren Antworten die in Klammern angegebenen Substantive im richtigen Fall.
+`;
+
+const questions = `
+1. Wessen Fotos sind das? (Anna, der Vater, die Mutter, das Kind, die Freunde)
+2. Wem antwortet die Mutter? (der Sohn, die Tochter, das Kind, die Geschwister)
+3. Wen fragst du? (ein Herr, eine Frau, ein Mädchen, unsere Kollegen)
+4. Was suchen Sie? (ein Rekorder, eine Tasche, ein Café, deutsche Bücher, Olgas Kamera)
+`;
+
+const audioFiles = `
+Wessen Fotos sind das.mp3
+Wem antwortet die Mutter.mp3
+Wen fragst du.mp3
+Was suchen Sie.mp3
+`;
+
+const answers = [
+`
+Das sind Annas Fotos.
+Das sind die Fotos des Vaters.
+Das sind die Fotos der Mutter.
+Das sind die Fotos des Kindes.
+Das sind die Fotos der Freunde.
+`,
+`
+Die Mutter antwortet dem Sohn.
+Die Mutter antwortet der Tochter.
+Die Mutter antwortet dem Kind.
+Die Mutter antwortet den Geschwistern.
+`,
+`
+Ich frage einen Herrn.
+Ich frage eine Frau.
+Ich frage ein Mädchen.
+Ich frage unsere Kollegen.
+`,
+`
+Ich suche einen Rekorder.
+Ich suche eine Tasche.
+Ich suche ein Café.
+Ich suche deutsche Bücher.
+Ich suche Olgas Kamera.
+`
+];
+
+const data = {
+    taskDescription,
+    questions,
+    audioFiles,
+    answers
+};
