153d182969ddb48a75d244840926e6802eb9318f
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2024-08-25 19:59:59 +0200

    update text

---
+++ text/Perfekt-Präteritum.js
@@ -0,0 +1,20 @@
+var text = `
+<Perfekt заверенное прош. время - разговорное>
+
+Ich stelle mein Buch ins Regal.
+Ich habe mein Buch ins Regal gestellt.
+
+Ich lege meinen Laptop auf den Tisch.
+Ich habe meinen Laptop auf den Tisch gelegt.
+
+Ich hänge mein T-Shirt in den Schrank.
+Ich habe mein T-Shirt in den Schrank gehängt.
+
+
+<Präteritum>
+
+Ich habe diese Sache zu Hause.
+Ich habe diese Sache zu Hause gehabt.   Perfekt
+Ich hatte diese Sache zu Hause.   Präteritum
+`;
+var audioFile = "Perfekt-Präteritum.mp3";
