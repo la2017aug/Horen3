c51f397ef18dcaba198426f8313c399063e94170
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2024-09-01 12:31:34 +0200

    Add files via upload

--- main.Portuguese.js
+++ main.Portuguese.js
@@ -6,20 +6,20 @@ See the LICENSE file for full terms.
 */
 
 const nodesData = `
-list	Ja-Nein.Portuguese	Ja-Nein
-list	Freue mich.Portuguese	Freue mich
-  list	Kommunikation.Portuguese	Kommunikation
-  list	Bekanntschaft.Portuguese	Bekanntschaft
-list	Wohnung.Portuguese	Wohnung
+list	Ja-Nein_Portuguese	Ja-Nein
+list	Freue mich_Portuguese	Freue mich
+  list	Kommunikation_Portuguese	Kommunikation
+  list	Bekanntschaft_Portuguese	Bekanntschaft
+list	Wohnung_Portuguese	Wohnung
   picture	Wohnung11	Wohnung 1-11
   picture	Stadt2	Stadt 1-2
   picture	Essen2	Essen 1-2
   picture	Kleidung2	Kleidung 1-2
-list	Sache1.Portuguese	Sache1
-list	Farbe.Portuguese	Farbe
-list	Gegensatz1.Portuguese	Gegensatz1 - On/Off
-list	Gegensatz2.Portuguese	Gegensatz2
-list	Verb1.Portuguese	Verben
+list	Sache1_Portuguese	Sache1
+list	Farbe_Portuguese	Farbe
+list	Gegensatz1_Portuguese	Gegensatz1 - On/Off
+list	Gegensatz2_Portuguese	Gegensatz2
+list	Verb1_Portuguese	Verben
 other	Language
   index	main.Portuguese	Portuguese
   index	main	Russian
