ae8b58d24976a0331987a9d7038b3e9136c799b2
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2024-09-04 20:44:04 +0200

    add Verb8_mit_Dativ.js

---
+++ list/Verb8_mit_Dativ.js
@@ -0,0 +1,38 @@
+// data.js
+// red purple yellow orange black brown gray blue green
+var LanguagePairEntry = `
+чем я могу вам уСлужить?  dienen	Womit kann ich Ihnen dienen?
+хочешь ты мне Угрожать?  drohen	Willst du mir etwa drohen?
+к твоей проблема к сожалению Приходить-на-ум ничего.  ein|fallen	Zu deinem Problem fällt mir leider auch nichts ein.
+почему бы тебе немного не Пойти-навстречу мне?  entgegen|kommen	Warum kommst du mir nicht ein bisschen entgegen?
+его покойной жены ему так Не-хватает.  fehlen	Seine verstorbene Frau fehlt ihm so sehr.
+пожалуйста, Следуйте за мной незаметно.  folgen	Bitte folgen Sie mir unauffällig.
+Нравится тебе блондинка там за стойкой?  gefallen	Gefällt dir die Blonde dort an der Theke?
+Все дети должны своих родителей Слушаться.  gehorchen	Alle Kinder müssen ihren Eltern gehorchen.
+Принадлежит корабль тебе?  gehören	Gehört das Schiff dir?
+торт твой очень хорошо Получился.  gelingen	Der Kuchen ist dir sehr gut gelungen.
+это Удовлетворяет(Достаточно) мне. Я остановлюсь.  genügen	Das genügt mir. Ich höre auf.
+его жена не Верит ему.  glauben	Seine Frau glaubt ihm nicht.
+коллеги поздравляют г-н Кляйбера к дню рождения.  gratulieren	Die Kollegen gratulieren Herrn Kleiber zum Geburtstag.
+морское путешествие станет тебе на пользу.  gut tun	Eine Seereise wird dir gut tun.
+Извините. Могли-бы Вы мне на-миг помочь?  helfen	Entschuldigung. Könnten Sie mir einen Augenblick helfen?
+Новый парень Астрид Не-нравится её подругам.  missfallen	Der neue Freund von Astrid missfällt ihren Freundinnen.
+Ураган Приближается очень быстро к маленькому острову.  sich nähern	Der Hurrikan nähert sich sehr schnell der kleinen Insel.
+он гонится(бежит за) за каждым трендом.  nach|laufen	Er läuft jedem Trend nach.
+это совсем мне не Приносит-пользы.  nützen	Das nützt mir gar nichts.
+новое платье Идет(подходит) твоей сестре очень.  passen	Das neue Kleid passt deiner Schwester sehr gut.
+такое со мной раньше еще никогда не Случалось.  passieren	Das ist mir vorher noch nie passiert.
+я[воротник] вот-вот Лопну(взорвусь) от гнева!  platzen	Mir platzt gleich der Kragen!
+я Советую тебе как-можно-скорее пойти к врачу.  raten	Ich rate dir schnellstens zum Arzt zu gehen.
+ты Вредишь только себе, когда ты лжешь.  sich schaden	Du schadest dir nur selbst, wenn du lügst.
+баварское пиво быть-По-вкусу моему отцу особенно.  schmecken	Das bayrische Bier schmeckt meinem Vater besonders gut.
+Лизелотта не Доверяет ее мужу больше.  vertrauen	Liselotte vertraut ihrem Mann nicht mehr.
+Прости меня ну только еще раз!  verzeihen	Verzeih mir nur noch das eine Mal!
+мой зуб ужасно Болит.  weh tun	Mein Zahn tut mir höllisch weh.
+не надо постоянно мне Возражать!  widersprechen	Du sollst mir nicht andauernd widersprechen!
+ты не можешь хотя бы раз меня Выслушать?  zu|hören	Kannst du mir nicht einmal zuhören?
+я не могу с твоим аргументом Согласиться.  zu|stimmen	Ich kann deiner Argumentation nicht zustimmen.
+`;
+
+// Подкаталог для MP3 файлов
+var subdirectory = 'audio/';
